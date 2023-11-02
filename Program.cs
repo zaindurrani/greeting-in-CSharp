8f3f7c40314ffe74b11ec8418bc6753ad8c27076
zain <s>
2023-11-02 08:03:55 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace firstc_
+{
+    internal class Program
+    {//just for practice
+
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Beep();
+            Console.WriteLine("Hello, everyone");
+
+            /*hile (true)
+             {
+                 Console.Beep();
+                        Console.SetCursorPosition(0, 0);
+
+             }*/
+            Console.WriteLine("Please enter your name :");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            Console.ReadKey();
+        }
+    }
+}
